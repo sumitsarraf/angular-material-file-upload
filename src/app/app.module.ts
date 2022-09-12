87c5b6a83d19abdea6305816a982357f35b4863f
Sumit Ranjan <34721357+sumitsarraf@users.noreply.github.com>
2022-09-12 11:11:33 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatListModule } from '@angular/material/list';
+
+const materialModules = [
+  MatCardModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatProgressBarModule,
+  MatListModule
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FileUploadComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    NoopAnimationsModule,
+    ...materialModules
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
